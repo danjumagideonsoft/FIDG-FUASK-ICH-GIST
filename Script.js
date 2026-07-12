cb2f6b801f315d2267eb5f819cd8c005a25f97fe
DANJUMA GIDEON <danjumagideon431@gmail.com>
2026-07-12 11:59:01 +0100

    Add initial script for FIDG registration system

---
+++ Script.js
@@ -0,0 +1,9 @@
+// FIDG - FUASK ICH Department Gist
+
+console.log("Welcome to FIDG!");
+
+const registerButton = document.querySelector("button");
+
+registerButton.addEventListener("click", function() {
+    alert("FIDG registration system coming soon!");
+});

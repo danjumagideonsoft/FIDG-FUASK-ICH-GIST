461235170a4e86b287c4b889773f212aa664c37c
DANJUMA GIDEON <danjumagideon431@gmail.com>
2026-07-12 19:40:55 +0100

    Initialize Firebase with configuration

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6zC_AflubTD7qQTqmpbpXKWci4qchhuc",
+  authDomain: "fidg-fuask-ich-gist.firebaseapp.com",
+  projectId: "fidg-fuask-ich-gist",
+  storageBucket: "fidg-fuask-ich-gist.firebasestorage.app",
+  messagingSenderId: "297541652176",
+  appId: "1:297541652176:web:ff5cd13dc4b0a018a458d5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

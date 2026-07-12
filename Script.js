28cf3105f800c52645c18e3dfaeb5f0dd60f6f24
DANJUMA GIDEON <danjumagideon431@gmail.com>
2026-07-12 13:06:49 +0100

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-// FIDG - FUASK ICH Department Gist
-
-console.log("Welcome to FIDG!");
-
-const registerButton = document.querySelector("button");
-
-registerButton.addEventListener("click", function() {
-    alert("FIDG registration system coming soon!");
-});
